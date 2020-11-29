23cc44c41d4ade6b48876400627d6e932aa3f211
zgmrclick <75209861+zgmrclick@users.noreply.github.com>
2020-11-29 16:00:14 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace New
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //int some = 7;
+            //some = 7;
+            //Console.WriteLine(some);
+            //some = 5;
+            //int cat = 40;
+            //Console.WriteLine(some);
+
+            int x = 3;
+            int y = 1000;
+            int z = 40;
+            if (x > y)
+            {
+                Console.WriteLine(x);
+            }
+            if (x > y)
+            {
+                Console.WriteLine(x);
+            }
+            else
+            {
+                Console.WriteLine(y);
+            }
+
+            //int a = int.Parse(Console.ReadLine());
+            //int b = int.Parse(Console.ReadLine());
+            //string myOperator = Console.ReadLine();
+            ////bool flag = myOperator == "+";
+            ////Console.WriteLine(flag);
+
+            //if (myOperator == "+")
+            //{
+            //    Console.WriteLine("result:" + (a + b));
+            //}
+            //else if (myOperator == "-")
+            //{
+            //    Console.WriteLine("result:" + (a - b));
+            //}
+            //else
+            //{
+            //    Console.WriteLine("mistake");
+            //}
+        }
+    }
+}
